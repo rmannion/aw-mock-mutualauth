d87ac0808844e961cbf36173c76811096d6dbb66
Ryan Mannion <rmannion@gmail.com>
2019-02-08 20:59:14 +0000

    Read/Delete basics

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Route } from 'react-router-dom';
+import {
+    CssBaseline,
+    withStyles,
+} from '@material-ui/core';
+import AppHeader from './components/AppHeader';
+import Home from './pages/Home';
+import Config from './pages/Config';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const styles = theme => ({
+    main: {
+	padding: 3 * theme.spacing.unit,
+	[theme.breakpoints.down('xs')]: {
+	    padding: 2 * theme.spacing.unit,
+	},
+    },
+});
 
-export default App;
+const App = ({ classes }) => (
+    <Fragment>
+	<CssBaseline />
+	<AppHeader />
+	<main className={classes.main}>
+	    <Config />
+	</main>
+    </Fragment>
+);
+
+export default withStyles(styles)(App);
+
---
+++ src/pages/Config.js
@@ -0,0 +1,116 @@
+import React, { Component, Fragment } from 'react';
+import {
+    withStyles,
+    Typography,
+    Button,
+    IconButton,
+    Paper,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemSecondaryAction,
+} from '@material-ui/core';
+import { compose } from 'recompose';
+import { withRouter, Route, Link } from 'react-router-dom';
+import { Delete as DeleteIcon, Add as AddIcon } from '@material-ui/icons';
+import moment from 'moment';
+
+const styles = theme => ({
+    tokens: {
+	marginTop: 2 * theme.spacing.unit,
+    },
+    fab: {
+	position: 'absolute',
+	bottom: 3 * theme.spacing.unit,
+	right: 3 * theme.spacing.unit,
+	[theme.breakpoints.down('xs')]: {
+	    bottom: 2 * theme.spacing.unit,
+	    right: 2 * theme.spacing.unit,
+	},
+    },
+});
+
+const API = process.env.REACT_APP_API || 'http://www.zdlocal.com:3001';
+
+class Config extends Component {
+    state = {
+	loading: true,
+	tokens: []
+    };
+
+    componentDidMount() {
+	this.getTokens();
+    }
+
+    async fetch(method, endpoint, body) {
+	try {
+	    const res = await fetch(`${API}${endpoint}`, {
+		method,
+		body: body && JSON.stringify(body),
+		headers: {
+		    'accept': 'application/json',
+		},
+	    });
+	    return await res.json();
+	} catch (error) {
+	    console.log(error);
+	}
+    }
+
+    async getTokens() {
+	this.setState({ loading: false, tokens: await this.fetch('get', '/token') });
+    }
+
+    async deleteToken(token) {
+	await this.fetch('delete', `/token/${token.mutualAuthToken}`);
+	this.getTokens();
+    }
+
+    render() {
+	const { classes } = this.props;
+
+	return (
+	    <Fragment>
+		<Typography variant="display1">Mutual Auth Tokens</Typography>
+
+		{this.state.tokens.length > 0 ? (
+		    <Paper elevation={1} className={classes.tokens}>
+			<List>
+			    {this.state.tokens.map(token => (
+				<ListItem key={token.mutualAuthToken} button component={Link} to={`/token/${token.mutualAuthToken}`}>
+				    <ListItemText
+					primary={token.mutualAuthToken}
+					secondary={token.dateCreated && `Updated ${moment(token.dateCreated).fromNow()}`}
+				    />
+				    <ListItemSecondaryAction>
+					<IconButton onClick={() => this.deleteToken(token)} color="inherit">
+					    <DeleteIcon />
+					</IconButton>
+				    </ListItemSecondaryAction>
+				</ListItem>
+			    ))}
+			</List>
+		    </Paper>
+		) : (
+		    this.state.loading == false && <Typography variant="subheading">No tokens</Typography>
+		)}
+
+		<Button
+		    variant="fab"
+		    color="secondary"
+		    aria-label="add"
+		    className={classes.fab}>
+		    <AddIcon />
+		</Button>
+
+		<Route exact path="/token/:id" render={this.renderTokenEditor} />
+	    </Fragment>
+	);
+    }
+}
+
+export default compose(
+    withRouter,
+    withStyles(styles),
+)(Config);
+
