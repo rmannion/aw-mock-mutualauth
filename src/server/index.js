394a7fd02d3736b7167bc810bff8e7ff97fea68b
Ryan Mannion <rmannion@gmail.com>
2019-02-08 20:42:09 +0000

    Store date modified

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,25 @@
 require('dotenv').config({ path: '.env.local' })
 var bodyParser = require('body-parser')
+var _ = require('lodash');
 
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 tokens = { 
-    'asdfasdf': 'yes'
+    'testMAT1': {
+	'dateCreated': Date(),
+	'consumerAuthToken': 'consumerAuthTokenA',
+    },
+    'testMAT2': {
+	'dateCreated': Date(),
+	'consumerAuthToken': 'consumerAuthTokenB',
+    },
 };
 
 app.use(async (req, res, next) => {
@@ -34,7 +44,10 @@ app.post('/token/:mutualAuthToken', (req, res) => {
     if (req.body.consumerAuthToken === undefined) {
 	res.status(400).send();
     } else {
-	tokens[req.params.mutualAuthToken] = req.body.consumerAuthToken;
+	tokens[req.params.mutualAuthToken] = {
+	    'dateCreated': Date(),
+	    'consumerAuthToken': req.body.consumerAuthToken,
+	};
 	res.status(200).send();
     }
 });
@@ -49,7 +62,10 @@ app.delete('/token/:mutualAuthToken', (req, res) => {
 });
 
 app.get('/token', (req, res) => {
-    res.status(200).send(tokens);
+    let response = _.map(tokens, function(value, key) {
+	return _.merge({'mutualAuthToken': key}, value);
+    });
+    res.status(200).send(response);
 });
 
 const port = process.env.SERVER_PORT || 3001;
