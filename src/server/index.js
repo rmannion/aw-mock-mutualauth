4c7c6ca4ee6f1fc9ce073dbdc878532311d91cfc
Ryan Mannion <rmannion@gmail.com>
2019-02-11 16:39:23 +0000

    Add event log

--- src/server/index.js
+++ src/server/index.js
@@ -11,69 +11,87 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-tokens = { 
+state = {
+  tokens: { 
     'testMAT1': {
-	'dateCreated': Date(),
-	'consumerAuthToken': 'consumerAuthTokenA',
+      'dateCreated': Date(),
+      'consumerAuthToken': 'consumerAuthTokenA',
     },
     'testMAT2': {
-	'dateCreated': Date(),
-	'consumerAuthToken': 'consumerAuthTokenB',
+      'dateCreated': Date(),
+      'consumerAuthToken': 'consumerAuthTokenB',
     },
     '*': {
-	'dateCreated': Date(),
-	'consumerAuthToken': 'fallback',
+      'dateCreated': Date(),
+      'consumerAuthToken': 'fallback',
     },
-};
+  },
+  events: [
+  ],
+}
 
 app.use(async (req, res, next) => {
-    try {
-	next();
-    } catch (error) {
-	next(error.message);
-    }
+  try {
+    next();
+  } catch (error) {
+    next(error.message);
+  }
 });
 
 app.post('/', (req, res) => {
-    let mutualAuthToken = req.header('useraccesstoken');
-    let token = tokens[mutualAuthToken] || tokens['*'];
-    if (token === undefined) {
-	res.status(404).send();
-    } else {
-	res.status(200).json({'userAccessToken': token.consumerAuthToken});
-    }
+  let mutualAuthToken = req.header('useraccesstoken');
+  let token = state.tokens[mutualAuthToken] || state.tokens['*'];
+
+  let event = {
+    date: Date(),
+    mutualAuthToken: mutualAuthToken,
+    consumerAuthToken: token && token.consumerAuthToken,
+    ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  };
+  state.events.unshift(event);
+
+  if (token === undefined) {
+    res.status(404).send();
+  } else {
+    res.status(200).json({'userAccessToken': token.consumerAuthToken});
+  }
 });
 
 app.post('/token/:mutualAuthToken', (req, res) => {
-    if (req.body.consumerAuthToken === undefined) {
-	res.status(400).send();
-    } else {
-	tokens[req.params.mutualAuthToken] = {
-	    'dateCreated': Date(),
-	    'consumerAuthToken': req.body.consumerAuthToken,
-	};
-	res.status(200).send();
-    }
+  if (req.body.consumerAuthToken === undefined) {
+    res.status(400).send();
+  } else {
+    state.tokens[req.params.mutualAuthToken] = {
+      'dateCreated': Date(),
+      'consumerAuthToken': req.body.consumerAuthToken,
+    };
+    res.status(200).send();
+  }
 });
 
 app.delete('/token/:mutualAuthToken', (req, res) => {
-    if (tokens[req.params.mutualAuthToken] === undefined) {
-	res.status(400).send();
-    } else {
-	delete tokens[req.params.mutualAuthToken];
-	res.status(200).send();
-    }
+  if (state.tokens[req.params.mutualAuthToken] === undefined) {
+    res.status(400).send();
+  } else {
+    delete state.tokens[req.params.mutualAuthToken];
+    res.status(200).send();
+  }
 });
 
 app.get('/token', (req, res) => {
-    let response = _.map(tokens, function(value, key) {
-	return _.merge({'mutualAuthToken': key}, value);
-    });
-    res.status(200).send(response);
+  let response = _.map(state.tokens, function(value, key) {
+    return _.merge({'mutualAuthToken': key}, value);
+  });
+  res.status(200).send(response);
+});
+
+app.get('/events', (req, res) => {
+  res.status(200).send(state.events);
 });
 
 const port = process.env.SERVER_PORT || 3001;
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
+
